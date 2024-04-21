007384b36356d1fc68651bb850239be908710581
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-21 17:59:13 +0530

    separate node details from the main fuction

--- src/createANode.js
+++ src/createANode.js
@@ -3,8 +3,9 @@ import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
 
-const main = async () => {
+const getNode = async () => {
     const node = await createLibp2p({
+        start: false,
         addresses: {
             listen: [
                 '/ip4/127.0.0.1/tcp/0'
@@ -13,18 +14,23 @@ const main = async () => {
         transports: [tcp()],
         connectionEncryption: [noise()],
         streamMuxers: [mplex()]
-    })
+    });
     
-    await node.start()
-    console.log('libp2p has started')
-    
-    console.log('listening on addresses:')
-    node.getMultiaddrs().forEach((addr) => {
-        console.log(addr.toString())
-    })
-    
-    await node.stop()
-    console.log('libp2p has stopped')
+    return node;
 }
 
-main().then().catch(console.error)
+const main = async () => {
+    const node = await getNode();
+    await node.start();
+    console.log('libp2p has started');
+    
+    console.log('listening on addresses:');
+    node.getMultiaddrs().forEach((addr) => {
+        console.log(addr.toString())
+    });
+    
+    await node.stop()
+    console.log('libp2p has stopped');
+}
+
+main().then().catch(console.error);
