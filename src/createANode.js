50b83b42e382041a4963ffd6fdd347859290fc3e
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-21 18:12:39 +0530

    Bed prepared for messaging

--- src/createANode.js
+++ src/createANode.js
@@ -1,7 +1,21 @@
-import { createLibp2p } from 'libp2p'
-import { tcp } from '@libp2p/tcp'
-import { noise } from '@chainsafe/libp2p-noise'
-import { mplex } from '@libp2p/mplex'
+import { createLibp2p } from 'libp2p';
+import { tcp } from '@libp2p/tcp';
+import { noise } from '@chainsafe/libp2p-noise';
+import { mplex } from '@libp2p/mplex';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = (query) => {
+    return new Promise(resolve => {
+        rl.question(query, (answer) => {
+            resolve(answer);
+        });
+    });
+};
 
 const getNode = async () => {
     const node = await createLibp2p({
@@ -28,6 +42,13 @@ const main = async () => {
     node.getMultiaddrs().forEach((addr) => {
         console.log(addr.toString())
     });
+    
+    const peeNext = await question('Enter the multiaddress: ');
+    var msg = "";
+    while (msg != "exit") {
+        msg = await question('Enter the message: ');
+    }
+    rl.close();
     
     await node.stop()
     console.log('libp2p has stopped');
